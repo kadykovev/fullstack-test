8864a6237627f007f44ce5f12fc44a618dc60552
Evgeniy Kadykov <kadykov_e_v@mail.ru>
2024-02-14 17:03:19 +0300

    Update HomeController.php

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -6,12 +6,6 @@ class HomeController extends BaseController
 {
 	public function index()
 	{
-		if ($r = $this->request->isAJAX()) {
-            print_r($this->request->getGet());
-            echo $r;
-		}
-		//$req = $this->request;
-		//dd($req);
 		return view('home');
 	}
 }
